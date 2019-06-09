b57fd41311c99588e5b72900e29fa1d758072ff6
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-06-09 18:13:17 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
-CREATE user 'web'@172.16.1.5 identified by 'viveleweb';
-CREATE user 'admin'@'localhost' identified by 'viveladministration';
+CREATE user 'web'@172.16.1.5 identified by 'xxx';
+CREATE user 'admin'@'localhost' identified by 'xxx';
 grant all privileges on *.* TO 'web'@172.16.1.5;
 grant all privileges on *.* TO 'admin'@'localhost';
 
